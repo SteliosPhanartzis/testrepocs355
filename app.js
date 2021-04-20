1a6b97e35f8f3cce10a72d6a4b60ecedb5b190c2
SteliosPhanartzis <stelios.phan@gmail.com>
2021-04-20 19:31:55 -0400

    changed for heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (request, response) => {
 		response.send('Hello World!');
 });
 
-app.listen(3000, () => {
-	console.log('App listening on port 3000');
+app.listen(PORT, () => {
+	console.log('App listening on port' + PORT);
 });
